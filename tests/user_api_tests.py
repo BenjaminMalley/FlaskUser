e64faee3910dbd96c8d8f60cfb6efec4c5de9655
Benjamin Malley <benjamin.malley@gmail.com>
2012-01-08 13:17:40 -0500

    added login test

--- tests/user_api_tests.py
+++ tests/user_api_tests.py
@@ -2,10 +2,10 @@ import unittest
 import sys
 import os
 from mongoengine import connect
+from flask import Flask
 import config #include mongolab configuration details; not included in repo for security reasons
 sys.path.append(os.path.abspath('..'))
-from models import User
-from flask import Flask
+from user_api import *
 
 class UserAPITestCase(unittest.TestCase):
 
@@ -19,8 +19,10 @@ class UserAPITestCase(unittest.TestCase):
 		super(UserAPITestCase, self).__init__(*args, **kwargs)
 	
 	def setUp(self):
-		self.app = Flask(__name__).test_client()
-	
+		self.app = Flask(__name__)
+		self.app.secret_key = 'test_key'
+		self.test_client = self.app.test_client()
+
 	def tearDown(self):
 		pass
 		
@@ -33,13 +35,24 @@ class UserAPITestCase(unittest.TestCase):
 		'''test that a password can be set and authenticated against'''
 		with User.objects(username='test_user')[0] as user:
 			user.set_password('test_password')
-			# a good password works
-			assert user.authenticate('test_password') == True
-			# and a bad one doesn't
-			assert user.authenticate('bad_password') == False
+		# a good password works
+		assert user.authenticated('test_password') == True
+		# and a bad one doesn't
+		assert user.authenticated('bad_password') == False
 
-	def test_login(self):
-		pass 
+
+	def test_user_login(self):
+		self.app.add_url_rule('/login/', view_func=LoginView.as_view('login'), methods=['POST',])
+		rv = self.test_client.post('/login/', data=dict(
+			username='test_user',
+			password='test_password'))
+		assert 'authenticated' in rv.data
+		rv = self.test_client.post('/login/', data=dict(
+			username='test_user',
+			password='bad_password'))
+		assert 'error' in rv.data
+
+
 			
 if __name__=='__main__':
 	unittest.main()
